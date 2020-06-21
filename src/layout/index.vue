d119dd1291047965201f1f1a15ec2b353b48c8d9
rgao <2236550035@qq.com>
2020-06-21 23:40:47 +0800

    0.1

---
+++ src/layout/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <el-menu-item index="/">首页</el-menu-item>
+  
+</template>
+
+<script>
+export default {
+    name:"TopSidebar"
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h2 class="nav">热门推荐</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+.nav{
+  border-bottom: 1px solid #1a1a1a;
+  padding-bottom: 12px;
+}
+</style>
